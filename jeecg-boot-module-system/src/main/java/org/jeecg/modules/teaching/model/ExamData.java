5cad9a92688bf047fd0d390e7337b3aba183932f
Fitz <924447939@qq.com>
2023-04-25 20:23:37 +0800

    \\

--- jeecg-boot-module-system/src/main/java/org/jeecg/modules/teaching/model/ExamData.java
+++ jeecg-boot-module-system/src/main/java/org/jeecg/modules/teaching/model/ExamData.java
@@ -6,5 +6,5 @@ import lombok.Data;
 public class ExamData {
     String[] programTypes={"Scratch","Python","机器人","C语言"};
     String[] levelTypes={"一级","二级","三级","四级","五级"};
-    String[] examTypes={"实际操作真题","理论综合真题","级真题"};
+    String[] examTypes={"实际操作真题","理论综合真题","级真题","理论综合测试卷"};
 }
